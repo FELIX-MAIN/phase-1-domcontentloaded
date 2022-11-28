6d6a88f9afd38e0103097eb34f2372490238eda4
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-29 01:05:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const handletext = document.querySelector("#text");
+   const addtext = document.createElement('p');
+
+   addtext.innerHTML = '<p id="text">This is really cool</p>';
+
+   handletext.parentNode.replaceChild(addtext, handletext);
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  
